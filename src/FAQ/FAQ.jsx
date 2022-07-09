8217b3bb8b1de44a49def5fa9670f5d76b8800cb
ksuess <k.suess@rohberg.ch>
2022-07-09 18:24:40 +0200

    clean up

--- src/FAQ/FAQ.jsx
+++ src/FAQ/FAQ.jsx
@@ -4,6 +4,7 @@ import { Icon } from '@plone/volto/components';
 import rightSVG from '@plone/volto/icons/right-key.svg';
 import downSVG from '@plone/volto/icons/down-key.svg';
 import AnimateHeight from 'react-animate-height';
+import './accordion.less';
 
 import { Accordion, Grid, Divider, Header } from 'semantic-ui-react';
 
@@ -12,12 +13,11 @@ const FAQ = ({ data }) => {
 
   return data.faq_list?.faqs ? (
     <>
-      <Divider section />
       {data.faq_list.faqs.map((id_qa) => (
         <Accordion key={id_qa} fluid exclusive={false}>
           <Accordion.Title
             index={id_qa}
-            className="stretched row"
+            className=""
             active={activeIndex.has(id_qa)}
             onClick={() => {
               const newSet = new Set(activeIndex);
@@ -25,49 +25,26 @@ const FAQ = ({ data }) => {
               setActiveIndex(newSet);
             }}
           >
-            <Grid>
-              <Grid.Row>
-                <Grid.Column width="1">
-                  {activeIndex.has(id_qa) ? (
-                    <Icon name={downSVG} size="20px" />
-                  ) : (
-                    <Icon name={rightSVG} size="20px" />
-                  )}
-                </Grid.Column>
-                <Grid.Column width="11">
-                  <Header as="h3">{data.faq_list.faqs_layout[id_qa][0]}</Header>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
+            {activeIndex.has(id_qa) ? (
+              <Icon name={downSVG} size="20px" />
+            ) : (
+              <Icon name={rightSVG} size="20px" />
+            )}
+            {data.faq_list.faqs_layout[id_qa][0]}
           </Accordion.Title>
-          <div>
-            <Accordion.Content
-              className="stretched row"
-              active={activeIndex.has(id_qa)}
+          <Accordion.Content active={activeIndex.has(id_qa)}>
+            <AnimateHeight
+              key={id_qa}
+              duration={300}
+              height={activeIndex.has(id_qa) ? 'auto' : 0}
             >
-              <Grid>
-                <Grid.Row>
-                  <Grid.Column width="1"></Grid.Column>
-                  <Grid.Column width="11">
-                    <div>
-                      <AnimateHeight
-                        key={id_qa}
-                        duration={300}
-                        height={activeIndex.has(id_qa) ? 'auto' : 0}
-                      >
-                        <div
-                          dangerouslySetInnerHTML={{
-                            __html: data.faq_list.faqs_layout[id_qa][1].data,
-                          }}
-                        />
-                      </AnimateHeight>
-                    </div>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            </Accordion.Content>
-          </div>
-          <Divider section />
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: data.faq_list.faqs_layout[id_qa][1].data,
+                }}
+              />
+            </AnimateHeight>
+          </Accordion.Content>
         </Accordion>
       ))}
     </>
